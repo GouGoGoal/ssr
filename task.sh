15969cd7bfbaa60bc3c9032940938207b1666e6b
NicolasG <37571306+GouGoGoal@users.noreply.github.com>
2020-10-04 19:56:11 +0800

    Update task.sh

--- task.sh
+++ task.sh
@@ -1,7 +1,1 @@
 #!/bin/bash
-
-sed -i '/TransientFailure/d' /etc/crontab
-echo "* * * * * root if [ \"\`journalctl -u v2ray -u v2ray@* -n 20|grep TransientFailure\`\" != \"\" ];then for line in \`systemctl|grep v2ray|grep -v system|awk '{print \$1}'\`;do systemctl restart \$line;done;fi" >>/etc/crontab
-
-
-sed -i 's/cn-state.soulout.club/state.soulout.club/g' /root/ssr/state.py
