9c938df069482cf1b9ef0a588fda5855248095c4
NicolasG <37571306+GouGoGoal@users.noreply.github.com>
2020-10-21 10:43:46 +0800

    添加paravi解锁

--- task.sh
+++ task.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-
+#添加paravi解锁
+if [ ! "`cat /etc/smartdns.sh|grep paravi`" ];then
+	sed  -i '/.*dmm.com.*/a\#日本Paravi\naddress /paravi.jp/$jpip' /etc/smartdns.sh 
+	rm -f /etc/smartdns.conf
+	rm -f /tmp/smartdns_tmp
+fi
+	
