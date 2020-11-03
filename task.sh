fefd49725a3f5c97f95c401061c2ccc9c6a7d290
NicolasG <37571306+GouGoGoal@users.noreply.github.com>
2020-11-03 09:38:42 +0800

    Update task.sh

--- task.sh
+++ task.sh
@@ -1,18 +1,1 @@
 #!/bin/bash
-
-
-#更新smartdns服务，让他开机就劫持所有DNS
-echo "[Unit]
-Description=SmartDNS server
-After=network.target NetworkManager.service
-Before=rc-local.service
-[Service]
-Type=simple
-ExecStartPre=`which iptables|tail -1` -t nat -A OUTPUT -p udp --dport 53 -j DNAT --to-destination 127.0.0.1:53
-ExecStart=`which smartdns|tail -1` -f -c /etc/smartdns.conf
-ExecStopPost=`which iptables|tail -1` -t nat -D OUTPUT -p udp --dport 53 -j DNAT --to-destination 127.0.0.1:53
-Restart=always
-[Install]
-WantedBy=multi-user.target" >/etc/systemd/system/smartdns.service
-#服务开机自启
-systemctl daemon-reload
