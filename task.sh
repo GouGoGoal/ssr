c963dd0a19890fc69e2c9fbfb621041a7168bf01
NicolasG <37571306+GouGoGoal@users.noreply.github.com>
2020-11-12 12:43:21 +0800

    让nginx不再输出日志

--- task.sh
+++ task.sh
@@ -4,6 +4,6 @@
 sed -i '/.*error_log.*/d' /etc/nginx/conf.d/default.conf
 
 #让nginx不输出日志
-if [ ! `cat /etc/nginx/nginx.conf|grep 'access_log /dev/null'` ];then
-sed  -i '/.*conf.d.*/ a\access_log /dev/null;\nerror_log /dev/null;' /etc/nginx/nginx.conf 
+if [ ! `cat /etc/nginx/nginx.conf|grep /dev/null` ];then
+sed  -i '/.*conf.d.*/ a\access_log off;\nerror_log /dev/null;' /etc/nginx/nginx.conf 
 fi
