a03c0635b8d6444eff7f056cd1a0c5edfd497a23
NicolasG <37571306+GouGoGoal@users.noreply.github.com>
2020-11-14 15:19:59 +0800

    一键脚本重写

--- task.sh
+++ task.sh
@@ -1,9 +1,1 @@
 #!/bin/bash
-
-
-sed -i '/.*error_log.*/d' /etc/nginx/conf.d/default.conf
-
-#让nginx不输出日志
-if [ ! `cat /etc/nginx/nginx.conf|grep /dev/null` ];then
-sed  -i '/.*conf.d.*/ a\access_log off;\nerror_log /dev/null;' /etc/nginx/nginx.conf 
-fi
