5d9737c2335f2a19b16ac4666b42ff9de50efcff
NicolasG <37571306+GouGoGoal@users.noreply.github.com>
2020-11-14 15:28:17 +0800

    一键脚本重构

--- setup.sh
+++ setup.sh
@@ -1,135 +1,31 @@
-#!/bin/sh
+#!/bin/bash
 
-if [ "$1" == '' ];then
-echo "未赋值，退出脚本"
-exit 0
+if [ "`id -u`" != 0 ];then
+	echo 'SB：请使用root用户执行脚本'
+	exit
 fi
-if [ "$2" == '' ];then
-echo "对接URL未填写"
-exit 0
+if [ ! -f "/etc/redhat-release" ];then
+	apt install -y python3 python3-pip git libsodium-dev vim curl libssl-dev swig ntp
+else
+	yum install -y python3 python3-pip git curl openssl-devel libffi libffi-dev ntp
 fi
-if [ "$2" == '' ];then
-echo "对接token未填写"
-exit 0
-fi
-
-#优先使用IPV6地址 
-echo "precedence ::ffff:0:0/96  100" >>/etc/gai.conf
-if [ ! -f "/etc/redhat-release" ]; then
-apt update
-#安装环境
-apt install -y python3 python3-pip git libsodium-dev vim libssl-dev swig ntp
-else 
-yum clean all
-yum makecache
-yum install -y python3 python3-pip git openssl-devel  libffi libffi-dev ntp
-#关闭防火墙
-systemctl disable firewalld
-systemctl stop firewalld
-#关闭 selinux
-setenforce 0
-echo 'SELINUX=disabled' >/etc/selinux/config
-fi
-#自动同步时间
-timedatectl set-ntp true
-#下载代码
-cd /root
-git clone -b manyuser https://github.com/GouGoGoal/ssr
-cd ssr
 pip3 install --upgrade setuptools 
 pip3 install cymysql requests pyOpenSSL ndg-httpsclient pyasn1 pycparser pycryptodome idna speedtest-cli
-mv apiconfig.py userapiconfig.py
-mv config.json user-config.json
 
-#添加服务
-echo "[Unit]
-Description=SSR deamon
-After=rc-local.service
-[Service]
-Type=simple
-ExecStart=/usr/bin/python3 /root/ssr/server.py
-Restart=always
-#启动频率限制，开启启动失败时尝试
-#StartLimitIntervalSec=180
-#StartLimitBurst=90
-LimitNOFILE=512000
-LimitNPROC=512000
-# 柔性限制
-# MemoryHigh=95%
-# 刚性限制
-# MemoryMax=25%
-[Install]
-WantedBy=multi-user.target">/etc/systemd/system/ssr.service
-
-if [ "$1" != '0' ];then
-#传入对接参数
-sed -i "s|node_id|$1|" userapiconfig.py
-sed -i "s|webapi_url|$2|" userapiconfig.py
-sed -i "s|webapi_token|$3|" userapiconfig.py
-
-systemctl enable ssr
-systemctl restart ssr
-fi
-
-#修改时区
-timedatectl set-timezone Asia/Shanghai
-#赋予脚本可执行权限
-chmod  +x /root/ssr/*.sh
-#计划任务改成bash执行
-sed -i 's|SHELL=/bin/sh|SHELL=/bin/bash|' /etc/crontab
-#添加计划任务
-echo '
-#每天05:55执行task
-55 5 * * * root curl -k https://raw.githubusercontent.com/GouGoGoal/ssr/manyuser/task.sh |bash
-#每天05:55清理日志日志
-55 5 * * * root find /var/ -name "*.log.*" -exec rm -rf {} \;
-#每天06:00点重启
-0 6 * * * root init 6
-'>>/etc/crontab
-rm -rf setup.sh .git .gitignore README.md 
-mv besttrace /usr/sbin
-chmod +x /usr/sbin/besttrace
-mv tcping /usr/sbin
-chmod +x /usr/sbin/tcping
-
-if [ "$4" != "0" -a "$4" != ""  ];then
-#添加探针服务
-echo "[Unit]
-Description=state deamon
-After=rc-local.service
-
-[Service]
-Type=simple
-ExecStart=/usr/bin/python3 /etc/state.py
-Restart=on-failure
-[Install]
-WantedBy=multi-user.target">/etc/systemd/system/state.service
-mv -f state.py /etc
-#传入参数
-sed -i "s|node|$4|" /etc/state.py
-systemctl enable state
-systemctl restart state
-echo "$4.lovegoogle.xyz已添加探针"
-fi
-
-if [ "$5" == "ovz" ];then
-echo "
-#关闭IPV6
-net.ipv6.conf.all.disable_ipv6 = 1
-#开启内核转发
-net.ipv4.ip_forward=1
-">/etc/sysctl.conf
-echo "已针对OVZ优化参数"
-exit 0
-fi
-
-#优化最大文件打开
-echo "
-root soft nofile 512000
-root hard nofile 512000
-">>/etc/security/limits.conf
-#优化TCP连接
-echo "
+cd /var
+rf -f ssr
+git clone -b manyuser https://github.com/GouGoGoal/ssr
+cd ssr
+#先循环一次，将带有-的参数进行配置
+for i in $*
+do
+	if [ "${i:0:1}" == '-' ];then 
+		i=${i:1}
+		A=`echo $i|awk -F '=' '{print $1}'`
+		case $A in 
+		#开BBR以及内核参数优化
+		bbr)
+			echo "
 #关闭IPV6
 net.ipv6.conf.all.disable_ipv6 = 1
 #开启BBR
@@ -163,10 +59,150 @@ net.ipv4.tcp_rmem = 4096 87380 67108864
 net.ipv4.tcp_wmem = 4096 65536 67108864
 ">/etc/sysctl.conf
 sysctl -p
+			;;
+		#自定义服务名
+		conf)
+			B=`echo $i|awk -F '=' '{print $2}'`
+			rm -rf /root/$B
+			mv -r ssr /root/$B
+			cd /root/$B
+			echo "[Unit]
+Description=SSR deamon
+After=rc-local.service
+[Service]
+Type=simple
+ExecStart=/usr/bin/python3 /root/$B/server.py
+Restart=always
+#启动频率限制，开启启动失败时尝试
+#StartLimitIntervalSec=180
+#StartLimitBurst=90
+LimitAS=infinity
+LimitRSS=infinity
+LimitCORE=infinity
+LimitNOFILE=999999
+[Install]
+WantedBy=multi-user.target">/etc/systemd/system/$B.service
+			conf=$B
+			;;
+		#监听端口
+		listen)
+			B=`echo $i|awk -F '=' '{print $2}'`
+			sed -i "s|^\"server\":.*|\"server\": \"$B\",|g" user-config.json
+			;;
+		#部署nginx实现tls加密		
+		nginx)
+			curl -k https://raw.githubusercontent.com/GouGoGoal/SHELL/master/Nginx/nginx.sh|bash
+			echo 'user  root;
+worker_processes  auto;
+worker_cpu_affinity auto;
+worker_rlimit_nofile 51200;
+error_log  /dev/null;
+pid        /var/run/nginx.pid;
+events {
+	use epoll;
+	multi_accept on;
+	worker_connections  2048;
+}
+stream {
+	access_log off;
+	error_log /dev/null;
+	server {
+		listen 8081 reuseport ssl;
+		listen 8081 udp;
+		proxy_pass 127.0.0.1:8080;
+		
+		ssl_certificate    /etc/nginx/tls/full_chain.pem;	
+		ssl_certificate_key    /etc/nginx/tls/private.key;
+		ssl_protocols       TLSv1.2 TLSv1.3;
+		ssl_prefer_server_ciphers on;
+		ssl_ciphers  ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-CHACHA20-POLY1305:ECDHE-RSA-CHACHA20-POLY1305:DHE-RSA-AES128-GCM-SHA256:DHE-RSA-AES256-GCM-SHA384;
+		tcp_nodelay on;
+	}
+}'>/etc/nginx/nginx.conf
+			systemctl enable nginx
+			systemctl start nginx
+			echo "#定时从github上更新tls证书
+50 5 * * 1 root wget -N --no-check-certificate -P /etc/nginx/tls https://raw.githubusercontent.com/GouGoGoal/v2ray/soga/full_chain.pem 
+50 5 * * 1 root wget -N --no-check-certificate -P /etc/nginx/tls https://raw.githubusercontent.com/GouGoGoal/v2ray/soga/private.key">>/etc/crontab
+			;;
+		#设置探针监控
+		state)
+			B=`echo $i|awk -F '=' '{print $2}'`
+			sed -i "s|^USER|USER = \"$B\"|g" state.py
+			mv -f state.py /etc/state.py
+			echo "[Unit]
+Description=state deamon
+After=rc-local.service
 
+[Service]
+Type=simple
+ExecStart=/usr/bin/python3 /etc/state.py
+Restart=on-failure
+[Install]
+WantedBy=multi-user.target">/etc/systemd/system/state.service
+			systemctl enable state
+			systemctl restart state
+			;;
+		#设置定时重启、定时清理日志等任务
+		task)
+			echo '
+#每天05:55执行task
+55 5 * * * root curl -k https://raw.githubusercontent.com/GouGoGoal/ssr/manyuser/task.sh|bash
+#每天05:55清理日志日志
+55 5 * * * root find /var/ -name "*.log.*" -exec rm -rf {} \;
+#每天06:00点重启
+0 6 * * * root init 6'>>/etc/crontab
+			;;
+		esac
+	fi
+done
 
-echo "针对kvm优化参数，已开启BBR，已修改启动时间"
-read -p "输入nodeID参数继续对接V2ray" v2_node
-if [ "$v2_node" != "" ]; then 
-bash <(wget --no-check-certificate -qO- 'https://raw.githubusercontent.com/GouGoGoal/v2ray/master/setup.sh') $v2_node $2 $3
+#如果没有指定-conf，则默认为systemctl status ssr
+if [ ! "$conf" ];then 
+	rm -f /root/ssr
+	mv -r /var/ssr /root/ssr
+	echo "[Unit]
+Description=SSR deamon
+After=rc-local.service
+[Service]
+Type=simple
+ExecStart=/usr/bin/python3 /root/ssr/server.py
+Restart=always
+#启动频率限制，开启启动失败时尝试
+#StartLimitIntervalSec=180
+#StartLimitBurst=90
+LimitAS=infinity
+LimitRSS=infinity
+LimitCORE=infinity
+LimitNOFILE=999999
+[Install]
+WantedBy=multi-user.target">/etc/systemd/system/ssr.service
+	conf=ssr
 fi
+
+#再循环一次，将不带-的参数的配置进行替换
+cd /root/$conf
+for i in $*
+do
+	if [ "${i:0:1}" == "-" ];then continue;fi
+	A=`echo $i|awk -F '=' '{print $1}'`
+	if [ "$A" == 'NODE_ID' -o "$A" == 'SPEEDTEST' -o "$A" == 'CLOUDSAFE' -o "$A" == 'ANTISSATTACK' -o "$A" == 'AUTOEXEC' ];then 
+		sed -i "s|^$A.*|$i|g" userapiconfig.py
+	else 
+		B=`echo $i|awk -F '=' '{print $2}'`
+		sed -i "s|^$A.*|$A='$B'|g" userapiconfig.py
+	fi
+done
+
+#启动服务
+systemctl daemon-reload
+systemctl enable $conf
+systemctl restart $conf
+echo '部署完毕，等待5秒将显示服务状态'
+sleep 5
+systemctl status $conf
+
+
+
+
+
